f569b4f40a44706308ef4167cfe32f863d4d0c00
Triara <triara.xiii@gmail.com>
2015-07-04 21:19:00 -0700

    debug: added trace to debug circleCI failure

--- src/storage/retrieveFromStorage.js
+++ src/storage/retrieveFromStorage.js
@@ -9,7 +9,7 @@ module.exports = filter => {
     return new Promise((resolve, reject) => {
 
         mongoClient.connect(url, (err, db) => {
-
+            console.log('err',err);
             const collection = db.collection(config.database.collectionName);
 
             collection.findOne(filter, (err, retrievedItem) => {
